671b5da6d30a30f77cb146f84d990f393c81c2b8
Matlo <mat.lau@laposte.net>
2012-02-28 22:29:20 +0000

    issue 140

---
+++ directories.h
@@ -0,0 +1,14 @@
+/*
+ * directories.h
+ *
+ *  Created on: 28 févr. 2012
+ *      Author: matlo
+ */
+
+#ifndef DIRECTORIES_H_
+#define DIRECTORIES_H_
+
+#define CONFIG_DIR "config/"
+#define CONFIG_EXAMPLE_DIR "config/example"
+
+#endif /* DIRECTORIES_H_ */
