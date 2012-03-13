e95f7f0d910646a83d01f9cbd3452d9beabd0853
Matlo <mat.lau@laposte.net>
2012-03-13 19:43:54 +0000

    Version 0.30

--- info.h
+++ info.h
@@ -1,6 +1,6 @@
 
-#define INFO_VERSION "0.25"
-#define INFO_YEAR "2010-2011"
+#define INFO_VERSION "0.30"
+#define INFO_YEAR "2010-2012"
 #define INFO_DEV "Matlo"
 #define INFO_WEB "http://gimx.fr"
 #define INFO_LICENCE "GNU GPL v3"
