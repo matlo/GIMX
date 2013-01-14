5035c7ad47d13b7b37a2b32e7a9b7adcf2f1b21e
Matlo <mat.lau@laposte.net>
2013-01-14 21:40:15 +0000

    GE windows test code.

--- shared/event/test/windows_test.c
+++ shared/event/test/windows_test.c
@@ -60,6 +60,8 @@ int main(int argc, char* argv[])
        }
      }
      
+     //do something periodically
+
      QueryPerformanceCounter(&t1);
 
      time_to_sleep = PERIOD - (t1.QuadPart - t0.QuadPart) * 1000000 / freq.QuadPart;
