262c864621dbb7b9d4cfabd7aa0fdaa67e7a08dc
Matlo <mat.lau@laposte.net>
2013-01-17 17:16:20 +0000

    Code cleaning

--- shared/configeditor/include/XmlWritter.h
+++ shared/configeditor/include/XmlWritter.h
@@ -15,16 +15,17 @@ class XmlWritter
         XmlWritter(ConfigurationFile* configFile);
         virtual ~XmlWritter();
         int WriteConfigFile();
+    protected:
+    private:
         void CreateEventNode(xmlNodePtr parent_node, Event* event);
         void CreateDeviceNode(xmlNodePtr parent_node, Device* device);
         void CreateAxisMapNode(xmlNodePtr parent_node);
         void CreateButtonMapNode(xmlNodePtr parent_node);
         void CreateTriggerNode(xmlNodePtr parent_node);
+        void CreateMouseOptionsNodes(xmlNodePtr parent_node);
         void CreateIntensityNodes(xmlNodePtr parent_node);
         void CreateConfigurationNodes(xmlNodePtr parent_node);
         void CreateControllerNodes(xmlNodePtr parent_node);
-    protected:
-    private:
         unsigned int m_CurrentConfiguration;
         unsigned int m_CurrentController;
         ConfigurationFile* m_ConfigurationFile;
