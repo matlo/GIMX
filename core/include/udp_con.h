a8d68ae84bb179b966bcc99801b814af39cec2c9
Matlo <mat.lau@laposte.net>
2013-12-01 21:51:32 +0100

    #105: Remote connection support

---
+++ core/include/udp_con.h
@@ -0,0 +1,14 @@
+/*
+ Copyright (c) 2013 Mathieu Laurendeau
+ License: GPLv3
+ */
+
+#ifndef UDP_CON_H_
+#define UDP_CON_H_
+
+int udp_listen(unsigned int ip, unsigned short port);
+int udp_connect(unsigned int ip, unsigned short port);
+unsigned int udp_send(int fd, unsigned char* buf, unsigned int len);
+void udp_close(int fd);
+
+#endif /* UDP_CON_H_ */
---
+++ core/udp_con.c
@@ -0,0 +1,66 @@
+/*
+ Copyright (c) 2013 Mathieu Laurendeau
+ License: GPLv3
+ */
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int udp_listen(unsigned int ip, unsigned short port)
+{
+  int fd;
+
+  if ((fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+  {
+    fprintf(stderr, "socket\n");
+    return -1;
+  }
+
+  struct sockaddr_in sa =
+  { .sin_family = AF_INET, .sin_port = htons(port), .sin_addr.s_addr = ip };
+
+  if (bind(fd, (struct sockaddr*)&sa, sizeof(sa)) == -1)
+  {
+    fprintf(stderr, "socket\n");
+    close(fd);
+    return -1;
+  }
+
+  return fd;
+}
+
+int udp_connect(unsigned int ip, unsigned short port)
+{
+  int fd;
+
+  if ((fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+  {
+    fprintf(stderr, "socket\n");
+    return -1;
+  }
+
+  struct sockaddr_in sa =
+  { .sin_family = AF_INET, .sin_port = htons(port), .sin_addr.s_addr = ip };
+
+  if (connect(fd, (struct sockaddr*)&sa, sizeof(sa)) == -1)
+  {
+    fprintf(stderr, "connect\n");
+    close(fd);
+    return -1;
+  }
+
+  return fd;
+}
+
+unsigned int udp_send(int fd, unsigned char* buf, unsigned int len)
+{
+  //TODO: fix flags
+  return send(fd, buf, len, MSG_DONTWAIT);
+}
+
+void udp_close(int fd)
+{
+  close(fd);
+}
