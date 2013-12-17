36aca6f569d27602b2ad7955a8b6a226b8698600
Matlo <mat.lau@laposte.net>
2013-12-18 00:03:38 +0100

    Code refactoring.

--- core/include/bt_utils.h
+++ core/include/bt_utils.h
@@ -6,7 +6,7 @@
 #ifndef BT_UTILS_H_
 #define BT_UTILS_H_
 
-int get_device_bdaddr(int, char*);
-int write_device_class(int);
+int get_device_bdaddr(int device_number, char bdaddr[18]);
+int write_device_class(int dev_number, uint32_t devclass);
 
 #endif /* BT_UTILS_H_ */
