110ea64ca35ffdd673bc2d9a466e9bc1fc18206b
matlo <mat.lau@laposte.net>
2013-12-31 15:30:38 +0100

    Write in a pcap file.

--- core/connectors/linux/sniffer.c
+++ core/connectors/linux/sniffer.c
@@ -16,6 +16,7 @@
 #include <termios.h>
 
 #include <sys/ioctl.h>
+#include <sys/time.h>
 
 #include <libintl.h>
 #define _(STRING)    gettext(STRING)
@@ -72,6 +73,94 @@ void serial_close(int fd)
   close(fd);
 }
 
+typedef struct pcap_hdr_s {
+  guint32 magic_number; /* magic number */
+  guint16 version_major; /* major version number */
+  guint16 version_minor; /* minor version number */
+  gint32 thiszone; /* GMT to local correction */
+  guint32 sigfigs; /* accuracy of timestamps */
+  guint32 snaplen; /* max length of captured packets, in octets */
+  guint32 network; /* data link type */
+} pcap_hdr_t;
+
+typedef struct pcaprec_hdr_s {
+  guint32 ts_sec; /* timestamp seconds */
+  guint32 ts_usec; /* timestamp microseconds */
+  guint32 incl_len; /* number of octets of packet saved in file */
+  guint32 orig_len; /* actual length of packet */
+} pcaprec_hdr_t;
+
+static pcap_hdr_t capture_header =
+{
+  .magic_number = 0xa1b2c3d4,
+  .version_major = 0x0002,
+  .version_minor = 0x0004,
+  .thiszone = 0x00000000,
+  .sigfigs = 0x00000000,
+  .snaplen = 0x0000FFFF,
+  .network = 0x000000C9, //DLT_BLUETOOTH_HCI_H4_WITH_PHDR
+};
+
+typedef struct _pcap_bluetooth_h4_header {
+  guint32 direction; /* if first bit is set direction is incoming */
+} pcap_bluetooth_h4_header;
+
+static pcaprec_hdr_t packet_header = {};
+
+static FILE* file = NULL;
+
+void pcapwriter_init(char* file_name)
+{
+  file = fopen(file_name, "w");
+
+  if(!file)
+  {
+    fprintf(stderr, "pcapwriter_init");
+  }
+  else
+  {
+    fwrite((char*)&capture_header, 1, sizeof(capture_header), file);
+  }
+}
+
+void pcapwriter_close()
+{
+  if(file)
+  {
+    fclose(file);
+  }
+}
+
+void pcapwriter_write(unsigned int direction, unsigned char packet_type, unsigned short data_length, unsigned char data[data_length])
+{
+  struct timeval tv;
+  unsigned int length = 0;
+
+  if(!file)
+  {
+    fprintf(stderr, "pcapwriter_write");
+    return;
+  }
+  
+  pcap_bluetooth_h4_header bt_h4_hdr =
+  {
+    .direction = dir
+  };
+
+  gettimeofday(&tv, NULL);
+
+  packet_header.ts_sec = tv.tv_sec;
+  packet_header.ts_usec = tv.tv_usec;
+
+  packet_header.incl_len = sizeof(bt_h4_hdr)+1+data_length;
+  packet_header.orig_len = sizeof(bt_h4_hdr)+1+data_length;
+
+  fwrite((char*)&packet_header, 1, sizeof(packet_header), file);
+  fwrite((char*)&bt_h4_hdr, 1, sizeof(bt_h4_hdr), file);
+  fwrite((char*)&type, 1, sizeof(type), file);
+  fwrite((char*)data, 1, data_length, file);
+}
+
 static volatile int done = 0;
 
 void terminate(int sig)
@@ -84,8 +173,21 @@ void terminate(int sig)
 #define SLIP_ESCAPE_START_END 0xDC
 #define SLIP_ESCAPE_ESCAPE    0xDD
 
+#define HCI_COMMAND_PKT         0x01
+#define HCI_ACLDATA_PKT         0x02
+#define HCI_SCODATA_PKT         0x03
+#define HCI_EVENT_PKT           0x04
+#define HCI_VENDOR_PKT          0x0E
+
 int main(int argc, char* argv[])
 {
+  if(argc < 2)
+  {
+    printf("usage: ./sniffer <filename>");
+  }
+  
+  pcapwriter_init(argv[1]);
+  
   int fd1 = serial_connect(PORT1);
   if(fd1 < 0)
   {
@@ -109,9 +211,12 @@ int main(int argc, char* argv[])
   unsigned char buf[sizeof(pfd)/sizeof(*pfd)][4096+4+2+2];
 
   int pos[sizeof(pfd)/sizeof(*pfd)] = {};
+  unsigned int direction[sizeof(pfd)/sizeof(*pfd)] = {};
   int res;
   int i;
   int escape = 0;
+  unsigned char type;
+  unsigned short length;
 
   while(!done)
   {
@@ -148,9 +253,21 @@ int main(int argc, char* argv[])
                 printf("got end of SLIP packet from fd=%d\n", pfd[i].fd);
                 printf("packet size: %d\n", pos[i]-1);
 
-                /*
-                 * TODO: decode and save packet
-                 */
+                type = buf[2] >> 4;
+                
+                switch(type)
+                {
+                  case HCI_COMMAND_PKT:
+                    direction[i] = 0;
+                    break;
+                  case HCI_EVENT_PKT:
+                    direction[i] = 1;
+                    break;
+                }
+                
+                length = ((buf[2] & 0x0F) << 8) + buf[3];
+                
+                pcapwriter_write(direction[i], type, length, buf);
 
                 pos[i] = 0;
               }
