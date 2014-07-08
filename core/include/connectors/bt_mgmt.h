be97d26448eb9d4eb6e7b891513a7c9c36b9f4b2
Matlo <mat.lau@laposte.net>
2014-07-08 19:22:04 +0200

    PS4 support.

---
+++ core/include/connectors/bt_mgmt.h
@@ -0,0 +1,13 @@
+/*
+ Copyright (c) 2014 Mathieu Laurendeau <mat.lau@laposte.net>
+ License: GPLv3
+ */
+
+#ifndef BT_MGMT_H
+#define BT_MGMT_H
+ 
+#include <stdint.h>
+
+int bt_mgmt_adapter_init(uint16_t index);
+
+#endif
