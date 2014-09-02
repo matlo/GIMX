8b270d4c2b9978e5d0b34a8f2b863d5b5d6fa067
Matlo <mat.lau@laposte.net>
2014-09-02 17:42:37 +0200

    #279

---
+++ core/include/stats.h
@@ -0,0 +1,13 @@
+/*
+ Copyright (c) 2014 Mathieu Laurendeau
+ License: GPLv3
+ */
+
+#ifndef STATS_H_
+#define STATS_H_
+
+void stats_init(int id);
+void stats_update(int id);
+int stats_get_frequency(int id);
+
+#endif /* STATS_H_ */
---
+++ core/stats.c
@@ -0,0 +1,70 @@
+/*
+ Copyright (c) 2014 Mathieu Laurendeau <mat.lau@laposte.net>
+ License: GPLv3
+ */
+
+#include <stddef.h>
+#ifdef WIN32
+#include <windows.h>
+#else
+#include <sys/time.h>
+#endif
+#include <adapter.h>
+
+#define STATS_PERIOD 500000 //ms
+
+#ifdef WIN32
+static LARGE_INTEGER freq;
+#endif
+
+static struct
+{
+#ifndef WIN32
+  struct timeval tlast;
+#else
+  LARGE_INTEGER tlast;
+#endif
+  int cpt;
+} stats[MAX_CONTROLLERS] = {};
+
+void stats_init(int id)
+{
+#ifndef WIN32
+  gettimeofday(&stats[id].tlast, NULL);
+#else
+  QueryPerformanceCounter(&stats[id].tlast);
+  QueryPerformanceFrequency(&freq);
+#endif
+}
+
+void stats_update(int id)
+{
+  stats[id].cpt++;
+}
+
+int stats_get_frequency(int id)
+{
+  int ret = -1;
+  int tdiff;
+
+#ifndef WIN32
+  struct timeval tnow;
+  gettimeofday(&tnow, NULL);
+
+  tdiff = (tnow.tv_sec * 1000000 + tnow.tv_usec) - (stats[id].tlast.tv_sec * 1000000 + stats[id].tlast.tv_usec);
+#else
+  LARGE_INTEGER t1;
+  QueryPerformanceCounter(&t1);
+
+  tdiff = (tnow.QuadPart - tlast.QuadPart) * 1000000 / freq.QuadPart;
+#endif
+
+  if(tdiff > STATS_PERIOD)
+  {
+    ret = stats[id].cpt*1000000/tdiff;
+    stats[id].tlast = tnow;
+    stats[id].cpt = 0;
+  }
+
+  return ret;
+}
