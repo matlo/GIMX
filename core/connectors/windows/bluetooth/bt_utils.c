e3d6fee74144cf62b6a15f9de1e44d00e79b3473
Matlo <mat.lau@laposte.net>
2014-12-07 12:48:18 +0100

    Definition of bdaddr_t for Windows.

--- core/connectors/windows/bluetooth/bt_utils.c
+++ core/connectors/windows/bluetooth/bt_utils.c
@@ -4,14 +4,62 @@
 */
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <connectors/bt_utils.h>
 
 #define HCI_REQ_TIMEOUT   1000
 
-#ifdef WIN32
-//TODO MLA
-//str2ba
-//ba2str
-#endif
+int bachk(const char *str)
+{
+  if (!str)
+    return -1;
+
+  if (strlen(str) != 17)
+    return -1;
+
+  while (*str) {
+    if (!isxdigit(*str++))
+      return -1;
+
+    if (!isxdigit(*str++))
+      return -1;
+
+    if (*str == 0)
+      break;
+
+    if (*str++ != ':')
+      return -1;
+  }
+
+  return 0;
+}
+
+int ba2str(const bdaddr_t *ba, char *str)
+{
+  return sprintf(str, "%2.2X:%2.2X:%2.2X:%2.2X:%2.2X:%2.2X",
+    ba->b[5], ba->b[4], ba->b[3], ba->b[2], ba->b[1], ba->b[0]);
+}
+
+int str2ba(const char *str, bdaddr_t *ba)
+{
+  bdaddr_t b;
+  int i;
+
+  if (bachk(str) < 0) {
+    memset(ba, 0, sizeof(*ba));
+    return -1;
+  }
+
+  for (i = 0; i < 6; i++, str += 3)
+    b.b[i] = strtol(str, NULL, 16);
+
+  baswap(ba, &b);
+
+  return 0;
+}
 
 /*
  * \brief This function gets the bluetooth device address for a given device number.
--- core/include/connectors/bt_utils.h
+++ core/include/connectors/bt_utils.h
@@ -6,6 +6,22 @@
 #ifndef BT_UTILS_H_
 #define BT_UTILS_H_
 
+#include <stdint.h>
+
+#ifdef WIN32
+/* BD Address */
+typedef struct {
+  uint8_t b[6];
+} __attribute__((packed)) bdaddr_t;
+
+int bachk(const char *str);
+int ba2str(const bdaddr_t *ba, char *str);
+int str2ba(const char *str, bdaddr_t *ba);
+
+int bt_connect();
+int bt_send_cmd(int fd, const hci_cmd_t *cmd, ...);
+#endif
+
 int bt_get_device_bdaddr(int device_number, char bdaddr[18]);
 int bt_write_device_class(int device_number, uint32_t devclass);
 int bt_disconnect(char bdaddr[18]);
