8ec89a57433e411aa1f95c233a071f2392873dd1
Matlo <mat.lau@laposte.net>
2015-02-17 22:00:06 +0100

    Code cleaning.

--- core/include/connectors/serial.h
+++ core/include/connectors/serial.h
@@ -6,9 +6,9 @@
 #ifndef SERIAL_H_
 #define SERIAL_H_
 
-int serial_connect(int id, char* portname);
+void serial_init();
+int serial_open(int id, char* portname);
 int serial_send(int id, void* pdata, unsigned int size);
-int serial_sendv(int id, void* pdata1, unsigned int size1, void* pdata2, unsigned int size2);
 int serial_recv(int id, void* pdata, unsigned int size);
 int serial_close(int id);
 
