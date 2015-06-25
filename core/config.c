ed7350205f22a925873b27f9727e8dbe8da3d0fa
Matlo <mat.lau@laposte.net>
2015-06-25 22:26:03 +0200

    #332

--- core/config.c
+++ core/config.c
@@ -22,34 +22,36 @@
 
 static unsigned int controller_dpi[MAX_CONTROLLERS];
 
-/*
- * This tells what's the current config of each controller.
- */
-static int current_config[MAX_CONTROLLERS];
-
-/*
- * This tells what's the next config of each controller.
- */
-static int next_config[MAX_CONTROLLERS];
-static int config_delay[MAX_CONTROLLERS];
-
-/*
- * This tells what's the previous config of each controller.
- * Hackish way to switch back to previous config.
- */
-static int previous_config[MAX_CONTROLLERS];
-
-/*
- * This lists config triggers for all controllers.
- */
-static struct
+typedef struct
 {
   int device_type;
   int device_id;
   int button;
-  int switch_back;
-  int delay; //ms
-} triggers[MAX_CONTROLLERS][MAX_CONFIGURATIONS];
+} s_event;
+
+typedef struct profile
+{
+  int index;
+  struct
+  {
+    s_event event;
+    int switch_back;
+    int delay; // in ms
+  } trigger;
+  struct
+  {
+    struct profile * previous;
+    struct profile * next;
+  } state;
+} s_profile;
+
+static struct
+{
+  s_profile * current;
+  s_profile * next;
+  int delay; // in periods
+  s_profile profiles[MAX_CONFIGURATIONS];
+} cfg_controllers[MAX_CONTROLLERS];
 
 /*
  * This lists controller stick intensity modifiers.
@@ -106,11 +108,11 @@ inline s_mapper_table* cfg_get_keyboard_buttons(int device, int controller, int 
 
 inline void cfg_set_trigger(s_config_entry* entry)
 {
-  triggers[entry->controller_id][entry->config_id].button = entry->event.id;
-  triggers[entry->controller_id][entry->config_id].device_id = entry->device.id;
-  triggers[entry->controller_id][entry->config_id].device_type = entry->device.type;
-  triggers[entry->controller_id][entry->config_id].switch_back = entry->params.trigger.switch_back;
-  triggers[entry->controller_id][entry->config_id].delay = entry->params.trigger.delay;
+  cfg_controllers[entry->controller_id].profiles[entry->config_id].trigger.event.button = entry->event.id;
+  cfg_controllers[entry->controller_id].profiles[entry->config_id].trigger.event.device_id = entry->device.id;
+  cfg_controllers[entry->controller_id].profiles[entry->config_id].trigger.event.device_type = entry->device.type;
+  cfg_controllers[entry->controller_id].profiles[entry->config_id].trigger.switch_back = entry->params.trigger.switch_back;
+  cfg_controllers[entry->controller_id].profiles[entry->config_id].trigger.delay = entry->params.trigger.delay;
 }
 
 inline void cfg_set_controller_dpi(int controller, unsigned int dpi)
@@ -337,7 +339,7 @@ void cfg_process_motion()
   for (i = 0; i < MAX_DEVICES; ++i)
   {
     mc = cfg_get_mouse_control(i);
-    mcal = cal_get_mouse(i, current_config[cal_get_controller(i)]);
+    mcal = cal_get_mouse(i, cfg_controllers[cal_get_controller(i)].current->index);
     if(!mc->change && mcal->options.mode == E_MOUSE_MODE_DRIVING)
     {
       //no auto-center
@@ -429,7 +431,7 @@ static void update_stick(int c_id, int axis)
     }
   }
   
-  s_intensity* intensity = &axis_intensity[c_id][current_config[c_id]][axis];
+  s_intensity* intensity = &axis_intensity[c_id][cfg_controllers[c_id].current->index][axis];
   double value = intensity->value;
 
   if(intensity->down_button == -1 && intensity->up_button == -1)
@@ -477,7 +479,7 @@ static int update_intensity(int device_type, int device_id, int button, int c_id
 {
   int ret = 0;
   
-  s_intensity* intensity = &axis_intensity[c_id][current_config[c_id]][axis];
+  s_intensity* intensity = &axis_intensity[c_id][cfg_controllers[c_id].current->index][axis];
 
   if (intensity->device_up_type == device_type && device_id == intensity->device_up_id && button == intensity->up_button)
   {
@@ -550,7 +552,7 @@ void cfg_intensity_lookup(GE_Event* e)
       if(update_intensity(device_type, device_id, button_id, c_id, a_id))
       {
         update_stick(c_id, a_id);
-        gprintf(_("controller %d configuration %d axis %s intensity: %.0f\n"), c_id, current_config[c_id], control_get_name(adapter_get(c_id)->type, a_id), axis_intensity[c_id][current_config[c_id]][a_id].value);
+        gprintf(_("controller %d configuration %d axis %s intensity: %.0f\n"), c_id, cfg_controllers[c_id].current->index, control_get_name(adapter_get(c_id)->type, a_id), axis_intensity[c_id][cfg_controllers[c_id].current->index][a_id].value);
       }
     }
   }
@@ -561,12 +563,71 @@ void cfg_intensity_lookup(GE_Event* e)
  */
 void cfg_trigger_init()
 {
-  int i;
+  int i, j;
   for(i=0; i<MAX_CONTROLLERS; ++i)
   {
-    next_config[i] = -1;
-    previous_config[i] = -1;
+    cfg_controllers[i].current = cfg_controllers[i].profiles;
+    cfg_controllers[i].next = NULL;
+    cfg_controllers[i].delay = 0;
+    for(j=0; j<MAX_CONFIGURATIONS; ++j)
+    {
+      s_profile * profile = cfg_controllers[i].profiles + j;
+      profile->index = j;
+      profile->state.next = NULL;
+      profile->state.previous = NULL;
+    }
   }
+}
+
+static inline s_event get_event(GE_Event * event)
+{
+  s_event e = {};
+
+  e.device_id = GE_GetDeviceId(event);
+
+  switch( event->type )
+  {
+    case GE_JOYBUTTONUP:
+      e.device_type = E_DEVICE_TYPE_JOYSTICK;
+      e.button = event->jbutton.button;
+      break;
+    case GE_JOYBUTTONDOWN:
+      e.device_type = E_DEVICE_TYPE_JOYSTICK;
+      e.button = event->jbutton.button;
+      break;
+    case GE_KEYUP:
+      e.device_type = E_DEVICE_TYPE_KEYBOARD;
+      e.button = event->key.keysym;
+      break;
+    case GE_KEYDOWN:
+      e.device_type = E_DEVICE_TYPE_KEYBOARD;
+      e.button = event->key.keysym;
+      break;
+    case GE_MOUSEBUTTONUP:
+      e.device_type = E_DEVICE_TYPE_MOUSE;
+      e.button = event->button.button;
+      break;
+    case GE_MOUSEBUTTONDOWN:
+      e.device_type = E_DEVICE_TYPE_MOUSE;
+      e.button = event->button.button;
+      break;
+    default:
+      break;
+  }
+
+  return e;
+}
+
+static inline int compare_trigger(s_profile * profile, s_event * event)
+{
+  if(event->device_type != profile->trigger.event.device_type
+      || event->device_id != profile->trigger.event.device_id
+      || event->button != profile->trigger.event.button)
+  {
+    return 1;
+  }
+
+  return 0;
 }
 
 /*
@@ -575,107 +636,103 @@ void cfg_trigger_init()
 void cfg_trigger_lookup(GE_Event* e)
 {
   int i, j;
-  int device_type;
-  int button;
   int up = 0;
-  int selected;
-  int current;
-  unsigned int device_id = GE_GetDeviceId(e);
+  s_profile * selected;
 
   switch( e->type )
   {
     case GE_JOYBUTTONUP:
-      up = 1;
-      device_type = E_DEVICE_TYPE_JOYSTICK;
-      button = e->jbutton.button;
-      break;
-    case GE_JOYBUTTONDOWN:
-      device_type = E_DEVICE_TYPE_JOYSTICK;
-      button = e->jbutton.button;
-      break;
     case GE_KEYUP:
-      up = 1;
-      device_type = E_DEVICE_TYPE_KEYBOARD;
-      button = e->key.keysym;
-      break;
-    case GE_KEYDOWN:
-      device_type = E_DEVICE_TYPE_KEYBOARD;
-      button = e->key.keysym;
-      break;
     case GE_MOUSEBUTTONUP:
       up = 1;
-      device_type = E_DEVICE_TYPE_MOUSE;
-      button = e->button.button;
       break;
+    case GE_JOYBUTTONDOWN:
+    case GE_KEYDOWN:
     case GE_MOUSEBUTTONDOWN:
-      device_type = E_DEVICE_TYPE_MOUSE;
-      button = e->button.button;
       break;
     default:
       return;
   }
 
+  s_event event = get_event(e);
+
   for(i=0; i<MAX_CONTROLLERS; ++i)
   {
-    selected = -1;
-    if(next_config[i] > -1)
+    selected = NULL;
+
+    s_profile * next = cfg_controllers[i].current;
+
+    if(cfg_controllers[i].next != NULL)
     {
-      current = next_config[i];
+      next = cfg_controllers[i].next;
     }
-    else
-    {
-      current = current_config[i];
-    }
+
     for(j=0; j<MAX_CONFIGURATIONS; ++j)
     {
-      if (triggers[i][j].device_type != device_type || device_id
-          != triggers[i][j].device_id)
+      s_profile * profile = cfg_controllers[i].profiles + j;
+
+      if (compare_trigger(profile, &event))
       {
         continue;
       }
-      if (button == triggers[i][j].button)
+
+      if (!up)
       {
-        if (!up)
+        if(next == profile)
         {
-          if(current == j)
-          {
-            continue;
-          }
-          if(selected < 0)
-          {
-            selected = j;
-          }
-          if(selected < current && j > current)
-          {
-            selected = j;
-          }
+          continue;
         }
-        else if(triggers[i][j].switch_back)
+        if(selected == NULL)
         {
-          if(next_config[i] == j)
+          selected = profile;
+        }
+        if(selected->index < next->index && j > next->index)
+        {
+          selected = profile;
+        }
+      }
+      else if(profile->trigger.switch_back)
+      {
+        if(cfg_controllers[i].next == profile)
+        {
+          /* cancel the switch */
+          cfg_controllers[i].next = NULL;
+          cfg_controllers[i].delay = 0;
+        }
+        else
+        {
+          s_profile * profile_next = profile->state.next;
+          if(profile_next)
           {
-            /* cancel the switch */
-            selected = current_config[i];
+            /* remove profile from history */
+            profile_next->state.previous = profile->state.previous;
+            s_profile * profile_previous = profile->state.previous;
+            if(profile_previous)
+            {
+              profile_previous->state.next = profile->state.next;
+            }
+            profile->state.next = NULL;
+            profile->state.previous = NULL;
           }
           else
           {
             /* switch back */
-            selected = previous_config[i];
+            selected = profile->state.previous;
           }
-          break;
         }
+        break;
       }
     }
-    if(selected > -1)
+    if(selected != NULL)
     {
-      next_config[i] = selected;
+      cfg_controllers[i].next = selected;
       if(!up)
       {
-        config_delay[i] = triggers[i][selected].delay / (gimx_params.refresh_period / 1000);
+        cfg_controllers[i].delay = selected->trigger.delay / (gimx_params.refresh_period / 1000);
       }
       else
       {
-        config_delay[i] = 0;
+        cfg_controllers[i].delay = 0;
       }
       break;
     }
@@ -692,30 +749,47 @@ void cfg_config_activation()
 
   for(i=0; i<MAX_CONTROLLERS; ++i)
   {
-    if(next_config[i] > -1)
+    s_profile * next = cfg_controllers[i].next;
+
+    if(next != NULL)
     {
-      if(!config_delay[i])
+      if(!cfg_controllers[i].delay)
       {
-        if(next_config[i] != current_config[i])
+        s_profile * current = cfg_controllers[i].current;
+
+        if(next != current)
         {
           if(gimx_params.status)
           {
             gettimeofday(&tv, NULL);
 
-            gprintf(_("%d %ld.%06ld controller %d is switched from configuration %d to %d\n"), i, tv.tv_sec, tv.tv_usec, i, current_config[i], next_config[i]);
+            gprintf(_("%d %ld.%06ld controller %d is switched from configuration %d to %d\n"), i, tv.tv_sec, tv.tv_usec, i, current->index, next->index);
           }
-          previous_config[i] = current_config[i];
-          current_config[i] = next_config[i];
+
+          if(current->state.previous != next)
+          {
+            current->state.next = next;
+            next->state.previous = current;
+          }
+          else
+          {
+            current->state.previous = NULL;
+            next->state.next = NULL;
+          }
+
+          cfg_controllers[i].current = next;
+
           for(j=0; j<AXIS_MAX; ++j)
           {
             update_stick(i, j);
           }
         }
-        next_config[i] = -1;
+
+        cfg_controllers[i].next = NULL;
       }
       else
       {
-        config_delay[i]--;
+        cfg_controllers[i].delay--;
       }
     }
   }
@@ -750,16 +824,17 @@ static int postpone_event(unsigned int device, GE_Event* event)
     }
   }
 
+  s_event e = get_event(event);
+
   for(i=0; i<MAX_CONTROLLERS; ++i)
   {
-    if(next_config[i] > -1)
+    s_profile * next = cfg_controllers[i].next;
+    if(next != NULL)
     {
-      if(triggers[i][next_config[i]].device_type == E_DEVICE_TYPE_MOUSE
-          && triggers[i][next_config[i]].device_id == device
-          && triggers[i][next_config[i]].button == event->button.button)
+      if(!compare_trigger(next, &e))
       {
         /* do not postpone the event if it has to trigger a switch back */
-        if(!triggers[i][next_config[i]].switch_back)
+        if(!next->trigger.switch_back)
         {
           GE_PushEvent(event);
           ret = 1;
@@ -905,7 +980,7 @@ static double mouse2axis(int device, s_adapter* controller, int which, double x,
 
 void update_dbutton_axis(s_mapper* mapper, int c_id, int axis)
 {
-  s_intensity* intensity = &axis_intensity[c_id][current_config[c_id]][axis];
+  s_intensity* intensity = &axis_intensity[c_id][cfg_controllers[c_id].current->index][axis];
   int value = intensity->value;
   if(mapper->axis_props.props & AXIS_PROP_NEGATIVE)
   {
@@ -948,7 +1023,7 @@ void update_dbutton_axis(s_mapper* mapper, int c_id, int axis)
 void update_ubutton_axis(s_mapper* mapper, int c_id, int axis)
 {
   int direction, opposite;
-  s_intensity* intensity = &axis_intensity[c_id][current_config[c_id]][axis];
+  s_intensity* intensity = &axis_intensity[c_id][cfg_controllers[c_id].current->index][axis];
   int value = intensity->value;
   adapter_get(c_id)->axis[axis] = 0;
   if(mapper->axis_props.props)
@@ -1014,7 +1089,7 @@ void cfg_process_event(GE_Event* event)
   for(c_id=0; c_id<MAX_CONTROLLERS; ++c_id)
   {
     controller = adapter_get(c_id);
-    config = current_config[c_id];
+    config = cfg_controllers[c_id].current->index;
 
     nb_controls = 0;
 
