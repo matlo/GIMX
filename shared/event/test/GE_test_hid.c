6bfdeb52f469eacc41f864aad47be32d9b1b2885
Matlo <mat.lau@laposte.net>
2015-10-23 23:53:44 +0200

    Improve the hidasync test program.

--- shared/event/test/GE_test_hid.c
+++ shared/event/test/GE_test_hid.c
@@ -28,6 +28,29 @@ static void terminate(int sig)
   done = 1;
 }
 
+#ifdef WIN32
+BOOL WINAPI ConsoleHandler(DWORD dwType)
+{
+    switch(dwType) {
+    case CTRL_CLOSE_EVENT:
+    case CTRL_LOGOFF_EVENT:
+    case CTRL_SHUTDOWN_EVENT:
+
+      done = 1;//signal the main thread to terminate
+
+      //Returning would make the process exit!
+      //We just make the handler sleep until the main thread exits,
+      //or until the maximum execution time for this handler is reached.
+      Sleep(10000);
+
+      return TRUE;
+    default:
+      break;
+    }
+    return FALSE;
+}
+#endif
+
 static void dump(const unsigned char * packet, unsigned char length)
 {
   int i;
@@ -46,12 +69,12 @@ int hid_read(int user, const void * buf, unsigned int count)
 {
   printf("read user: %d\n", user);
   dump((unsigned char *)buf, count);
+  fflush(stdout);
   return 0;
 }
 
 int hid_write(int user)
 {
-  printf("write user: %d\n", user);
   return 0;
 }
 
@@ -59,6 +82,11 @@ int hid_close(int user)
 {
   printf("close user: %d\n", user);
   done = 1;
+  return 0;
+}
+
+int ignore_event(GE_Event* event)
+{
   return 0;
 }
 
@@ -71,10 +99,7 @@ int main(int argc, char* argv[])
   }
 
   (void) signal(SIGINT, terminate);
-
-#ifndef WIN32
-  setlinebuf(stdout);
-#endif
+  (void) signal(SIGTERM, terminate);
 
   //Open Logitech RumblePad 2
   int hid = hidasync_open_ids(0x046d, 0xc218);
@@ -83,7 +108,7 @@ int main(int argc, char* argv[])
   {
     if(hidasync_register(hid, 42, hid_read, hid_write, hid_close, REGISTER_FUNCTION) != -1)
     {
-      GE_SetCallback(process_event);
+      GE_SetCallback(ignore_event);
 
       GE_TimerStart(PERIOD);
 
@@ -94,24 +119,34 @@ int main(int argc, char* argv[])
         //do something periodically
 
         static int i = 1;
-        if(i == 1) {
-          //Download and Play Force               weak        strong
-          unsigned char ff[] = {0x00, 0x51, 0x00, 0x7f, 0x00, 0x7f, 0x00, 0x00};
-          hidasync_write(hid, ff, sizeof(ff));
-        }
-        else if(i == 100) {
+
+        if(i == 200) {
+          i = 0;
+        } else if(i >= 100) {
+          if(i == 100) {
+            printf("stop\n");
+            fflush(stdout);
+          }
           //Stop Force
           unsigned char stop[] = {0x00, 0xf3};
           hidasync_write(hid, stop, sizeof(stop));
         }
-        else if(i == 200) {
-          i = 0;
+        else if(i >= 1) {
+          if(i == 1) {
+            printf("play\n");
+            fflush(stdout);
+          }
+          //Download and Play Force               weak        strong
+          unsigned char ff[] = {0x00, 0x51, 0x00, 0x7f, 0x00, 0x7f, 0x00, 0x00};
+          hidasync_write(hid, ff, sizeof(ff));
         }
         ++i;
       }
 
       GE_TimerClose();
 
+      printf("stop\n");
+      fflush(stdout);
       //Stop Force
       unsigned char stop[] = {0x00, 0xf3};
       hidasync_write_timeout(hid, stop, sizeof(stop), 1);
