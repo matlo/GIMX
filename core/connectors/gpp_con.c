8062117bdcf399488583d13a8cb961208fd0af50
Matlo <mat.lau@laposte.net>
2015-11-11 11:41:36 +0100

    [GPP/Cronus/Titan] Mouse to axis translation defects when reaching max stick positions #360

--- core/connectors/gpp_con.c
+++ core/connectors/gpp_con.c
@@ -45,7 +45,7 @@ int gpp_connect(int id, const char* device)
   return ret;
 }
 
-inline int8_t scale_axis(e_controller_type type, int index, int axis[AXIS_MAX])
+inline int scale_axis(e_controller_type type, int index, int axis[AXIS_MAX])
 {
   return axis[index] * 100 / controller_get_max_signed(type, index);
 }
