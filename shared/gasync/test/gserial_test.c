195de0f3991fcd2b21acd197e51a73d2535b0054
Matlo <mat.lau@laposte.net>
2016-01-04 23:04:28 +0100

    Improve the gserial test.

--- shared/gasync/test/gserial_test.c
+++ shared/gasync/test/gserial_test.c
@@ -8,11 +8,14 @@
 #include <signal.h>
 #include <errno.h>
 #include <string.h>
+#include <math.h>
+#include <getopt.h>
 
 #include <gserial.h>
 #include <ginput.h>
 #include <gpoll.h>
 #include <gtimer.h>
+#include <gprio.h>
 #include "common.h"
 
 #define PERIOD 10000//microseconds
@@ -27,22 +30,97 @@ static void terminate(int sig) {
   done = 1;
 }
 
-static void dump(const unsigned char * packet, unsigned char length) {
-  int i;
-  for (i = 0; i < length; ++i) {
-    if (i && !(i % 8)) {
-      printf("\n");
-    }
-    printf("0x%02x ", packet[i]);
-  }
-  printf("\n");
-}
+static char * port = NULL;
 
 static int serial = -1;
 
-static unsigned char packet[64] = { };
-static unsigned char result[sizeof(packet)] = { };
-static unsigned char read = 0;
+static unsigned char * packet;
+static unsigned char * result;
+static unsigned int read = 0;
+static unsigned int count = 0;
+
+static unsigned int baudrate = 0;
+static unsigned int samples = 0;
+static unsigned short packet_size = 0;
+
+static unsigned int verbose = 0;
+
+static struct timeval t0, t1;
+
+static unsigned int * tRead = NULL;
+static unsigned int wread = 0;
+
+void results(unsigned int * tdiff, unsigned int cpt) {
+  unsigned int sum = 0;
+  unsigned int average;
+  unsigned int temp = 0;
+  unsigned int nbval;
+  unsigned int worst = 0;
+
+  unsigned int i;
+  for (i = 0; i < cpt && tdiff[i] > 0; i++) {
+    sum = sum + tdiff[i];
+    if (tdiff[i] > worst) {
+      worst = tdiff[i];
+    }
+  }
+
+  nbval = i;
+
+  printf("%d\t", worst);
+
+  if (nbval < 2) {
+    return;
+  }
+
+  average = sum / nbval;
+
+  printf("%d\t", average);
+
+  for (i = 0; i < nbval; i++) {
+    temp = temp + pow(abs(tdiff[i] - average), 2);
+  }
+
+  temp = pow(temp / (nbval - 1), 0.5);
+
+  printf("%d\t", temp);
+}
+
+static void usage() {
+  fprintf(stderr, "Usage: ./serial_bench [-p port] [-b baudrate] [-n samples] [-s packet size] -v\n");
+  exit(EXIT_FAILURE);
+}
+
+/*
+ * Reads command-line arguments.
+ */
+static int read_args(int argc, char* argv[]) {
+
+  int opt;
+  while ((opt = getopt(argc, argv, "b:n:p:s:v")) != -1) {
+    switch (opt) {
+    case 'b':
+      baudrate = atoi(optarg);
+      break;
+    case 'n':
+      samples = atoi(optarg);
+      break;
+    case 'p':
+      port = optarg;
+      break;
+    case 's':
+      packet_size = atoi(optarg);
+      break;
+    case 'v':
+      verbose = 1;
+      break;
+    default: /* '?' */
+      usage();
+      break;
+    }
+  }
+  return 0;
+}
 
 int serial_read(int user, const void * buf, int status) {
 
@@ -54,81 +132,122 @@ int serial_read(int user, const void * buf, int status) {
   memcpy(result + read, buf, status);
   read += status;
 
-  gserial_set_read_size(serial, sizeof(packet) - read);
+  gserial_set_read_size(serial, packet_size - read);
 
-  if (read < sizeof(packet)) {
+  if (read < packet_size) {
     return 0;
   }
 
-  printf("user: %d\n", user);
-  dump((unsigned char *) result, sizeof(packet));
+  gettimeofday(&t1, NULL );
 
-  if (memcmp(result, packet, sizeof(packet))) {
+  int ret = 0;
+
+  if (memcmp(result, packet, packet_size)) {
+
     fprintf(stderr, "bad packet content\n");
     done = 1;
+    ret = -1;
+
   } else {
+
     unsigned int i;
-    for (i = 0; i < sizeof(packet); ++i) {
+    for (i = 0; i < packet_size; ++i) {
       packet[i]++;
     }
-    gserial_write(serial, packet, sizeof(packet));
+
+    tRead[count] = (t1.tv_sec * 1000000 + t1.tv_usec) - (t0.tv_sec * 1000000 + t0.tv_usec);
+
+    if (tRead[count] > wread) {
+      wread = tRead[count];
+    }
+
+    ++count;
+    if (count == samples) {
+
+      done = 1;
+
+    } else {
+
+      int status = gserial_write(serial, packet, packet_size);
+      if (status < 0) {
+        done = 1;
+      }
+
+      gettimeofday(&t0, NULL );
+    }
 
     read = 0;
-    gserial_set_read_size(serial, sizeof(packet));
   }
 
-  return 0;
+  if (done) {
+    ret = -1;
+  }
+
+  return ret;
 }
 
 int serial_close(int user) {
-  printf("close user: %d\n", user);
-  return 0;
+  done = 1;
+  return 1;
 }
 
 int main(int argc, char* argv[]) {
-  if (!ginput_init(GE_MKB_SOURCE_NONE, process_event)) {
-    fprintf(stderr, "GE_initialize failed\n");
-    exit(-1);
-  }
 
   (void) signal(SIGINT, terminate);
 
-#ifndef WIN32
-  setlinebuf(stdout);
-#endif
+  read_args(argc, argv);
 
-  serial = gserial_open("/dev/ttyUSB0", 500000);
-
-  if (serial >= 0) {
-    gserial_register(serial, 42, serial_read, NULL, serial_close, REGISTER_FUNCTION);
-
-    gserial_set_read_size(serial, sizeof(packet));
-
-    int timer = gtimer_start(42, PERIOD, timer_read, timer_close, REGISTER_FUNCTION);
-    if (timer < 0) {
-      done = 1;
-    }
-
-    gserial_write(serial, packet, sizeof(packet));
-
-    while (!done) {
-      gpoll();
-
-      //do something periodically
-    }
-
-    if (timer >= 0) {
-      gtimer_close(timer);
-    }
-
-    gserial_close(serial);
-  } else {
-    fprintf(stderr, "error opening serial device\n");
+  if(port == NULL || baudrate == 0 || samples == 0 || packet_size == 0)
+  {
+    usage();
+    return -1;
   }
 
-  ginput_quit();
+  packet = calloc(packet_size, sizeof(*packet));
+  result = calloc(packet_size, sizeof(*result));
 
-  printf("Exiting\n");
+  tRead = calloc(samples, sizeof(*tRead));
+
+  if(packet == NULL || result == NULL || tRead == NULL) {
+    fprintf(stderr, "can't allocate memory to store samples\n");
+    return -1;
+  }
+
+  if (gprio() < 0) {
+    return -1;
+  }
+
+  serial = gserial_open(port, baudrate);
+  if (serial < 0) {
+    return -1;
+  }
+
+  gserial_register(serial, 42, serial_read, NULL, serial_close, REGISTER_FUNCTION);
+
+  gserial_set_read_size(serial, packet_size);
+
+  int ret = gserial_write(serial, packet, packet_size);
+  if (ret < 0) {
+    done = 1;
+  }
+
+  gettimeofday(&t0, NULL );
+
+  while (!done) {
+
+    gpoll();
+  }
+
+  gserial_close(serial);
+
+  if(verbose) {
+    printf("baudrate: %d ", baudrate);
+    printf("samples: %d ", count);
+    printf("packet size: %d\n", packet_size);
+    printf("worst\tavg\tstdev\n");
+  }
+  results(tRead, count);
+  printf("\n");
 
   return 0;
 }
