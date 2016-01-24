b09d28703c54eaede7a6c8f0344002a7d3105222
Matlo <mat.lau@laposte.net>
2016-01-24 07:48:22 +0100

    Fix gtimer_test for Windows.

--- shared/gasync/test/gtimer_test.c
+++ shared/gasync/test/gtimer_test.c
@@ -21,6 +21,28 @@
 #define REGISTER_FUNCTION gpoll_register_fd
 #endif
 
+#ifdef WIN32
+# define timeradd(a, b, result)                                               \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec + (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec + (b)->tv_usec;                          \
+    if ((result)->tv_usec >= 1000000)                                         \
+      {                                                                       \
+        ++(result)->tv_sec;                                                   \
+        (result)->tv_usec -= 1000000;                                         \
+      }                                                                       \
+  } while (0)
+# define timersub(a, b, result)                                               \
+  do {                                                                        \
+    (result)->tv_sec = (a)->tv_sec - (b)->tv_sec;                             \
+    (result)->tv_usec = (a)->tv_usec - (b)->tv_usec;                          \
+    if ((result)->tv_usec < 0) {                                              \
+      --(result)->tv_sec;                                                     \
+      (result)->tv_usec += 1000000;                                           \
+    }                                                                         \
+  } while (0)
+#endif
+
 static volatile int done = 0;
 
 static struct {
