63f2fbd6e3a38f4deb715f19318ac22e5b7aa036
matlo <mat.lau@laposte.net>
2020-04-24 23:15:40 +0200

    Bump version to 8.0.

--- info.h
+++ info.h
@@ -1,8 +1,8 @@
 #ifndef INFO_H_
 #define INFO_H_
 
-#define INFO_VERSION "7.15"
-#define INFO_YEAR "2010-2019"
+#define INFO_VERSION "8.0"
+#define INFO_YEAR "2010-2020"
 #define INFO_DEV "Matlo"
 #define INFO_WEB "http://gimx.fr"
 #define INFO_LICENCE "GNU GPL v3"
